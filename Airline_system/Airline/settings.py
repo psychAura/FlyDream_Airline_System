ce8fb1d6c35b8713725fa93a8bc81232b577228f
psychaura <kausara931@gmail.com>
2025-07-29 16:57:29 +0000

    changes

--- Airline_system/Airline/settings.py
+++ Airline_system/Airline/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'Airline.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default= 'sqlite:///db.sqlite3')
 }
 
 
